0c52a86ddc4fcdc500fce7d7ad7954a7c4099cde
Sabhya Rajvanshi <149665798+RajvanshiSabhya@users.noreply.github.com>
2025-11-05 17:53:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -202,5 +202,7 @@ async def predict_batch(files: List[UploadFile] = File(...)):
 if __name__ == "__main__":
     import os
     import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    print(f"🚀 Starting server on port {port}")
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
